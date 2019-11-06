3a99d3b3585fb54c9394e4df0d883147b0e29004
William Wong <compulim@hotmail.com>
2019-11-06 02:03:24 -0800

    Reduce transpilation for Jest

---
+++ packages/component/.babelrc.js
@@ -0,0 +1,31 @@
+module.exports = api => {
+  if (api.env('test')) {
+    return {
+      plugins: [
+        '@babel/plugin-proposal-object-rest-spread'
+      ],
+      presets: [
+        ['@babel/preset-env', {
+          targets: {
+            node: 'current'
+          }
+        }],
+        '@babel/preset-react'
+      ]
+    };
+  }
+
+  return {
+    plugins: [
+      '@babel/plugin-proposal-object-rest-spread',
+      '@babel/plugin-transform-runtime'
+    ],
+    presets: [
+      ['@babel/preset-env', {
+        forceAllTransforms: true,
+        modules: 'commonjs'
+      }],
+      '@babel/preset-react'
+    ]
+  };
+};
