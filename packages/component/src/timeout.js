3098a064537d4779c34833b64172c7b74a1bc80c
William Wong <compulim@hotmail.com>
2019-11-06 02:13:32 -0800

    Use sleep internally

--- packages/component/src/timeout.js
+++ packages/component/src/timeout.js
@@ -1,3 +1,5 @@
-export default function (ms) {
-  return new Promise((_, reject) => setTimeout(() => reject(new Error('timed out')), ms));
+import sleep from './sleep';
+
+export default function timeout(ms) {
+  return sleep(ms).then(() => Promise.reject(new Error('timed out')));
 }
