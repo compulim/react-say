837e3104d8b55f70fb55e3fd78265637ba727459
William Wong <compulim@users.noreply.github.com>
2025-12-22 08:35:55 +0000

    Revert

--- packages/react-say/tsup.config.ts
+++ packages/react-say/tsup.config.ts
@@ -4,10 +4,13 @@ export default defineConfig([
   {
     dts: true,
     entry: {
-      'react-say': './src/index.ts'
+      'react-say': './src/index.mjs'
     },
     format: ['cjs', 'esm'],
     sourcemap: true,
-    target: 'esnext'
+    target: 'esnext',
+
+    // Remove the followings after we ported to TypeScript.
+    loader: { '.js': 'jsx' }
   }
 ]);
